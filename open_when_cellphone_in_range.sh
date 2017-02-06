3b809d7a158c56791914d1c5125db7d62f8f1d92
Maxi Padulo <maxi.padulo@gmail.com>
2017-02-06 21:55:35 +0100

    Micro refactor

--- open_when_cellphone_in_range.sh
+++ open_when_cellphone_in_range.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
+# 
+# DISCLAIMER: This is just a proof of concept
+#   For god sake, I'm using the IP of the phone!
+#
 OPENDOR_BIN="python ./opendoor.py"
 PING_DEBUG=/tmp/pingdebug.log
+CELLPHONE_IP=192.168.1.54
 
+echo "$(date): will wait until the pone goes away"
 while true; do
     ((count = 4))                
-    while [[ $count -ne 0 ]] && ! ping -c 1 192.168.1.54 >$PING_DEBUG; do
+    while [[ $count -ne 0 ]] && ! ping -c1 $CELLPHONE_IP >$PING_DEBUG; do
         ((count = count - 1)) 
         sleep 1
         echo "$count"
@@ -18,7 +23,7 @@ while true; do
     fi
 
     echo "$(date): long time gone, if appears i will open it right away"
-    while ! ping -c 1 192.168.1.54 >$PING_DEBUG; do
+    while ! ping -c1 $CELLPHONE_IP >$PING_DEBUG; do
         sleep 1
     done
 
